8b1324f67dc902e0817459fbbee48a46777829ae
RedScorpio <krzysiekmie@gmail.com>
2014-11-25 14:02:40 +0100

    Wall. Removed not needed todos. Added important todos.

--- ITAD/src/pl/polsl/dotnet/itacademicday/layouts/MainActivity.java
+++ ITAD/src/pl/polsl/dotnet/itacademicday/layouts/MainActivity.java
@@ -1,6 +1,9 @@
 package pl.polsl.dotnet.itacademicday.layouts;
 
 import pl.polsl.dotnet.itacademicday.R;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -21,11 +24,19 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 	 */
 	private CharSequence mTitle;
 
+	private static ConnectivityManager mConManager;
+
+	public static boolean hasAccessToNetwork(){
+
+		final NetworkInfo activeNetwork = mConManager.getActiveNetworkInfo();
+		return (activeNetwork != null && activeNetwork.isConnected());
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		mConManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(
 				R.id.navigation_drawer);
 		mTitle = getTitle();
@@ -35,22 +46,31 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 	}
 
 	@Override
+	protected void onResume(){
+		super.onResume();
+		if (mConManager == null)
+			mConManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	}
+
+	@Override
 	public void onNavigationDrawerItemSelected(int position){
 		// update the main content by replacing fragments
 		FragmentManager fragmentManager = getSupportFragmentManager();
+		position++;
 		Fragment f = null;
 		switch (position) {
-			case 0:
-				f = AboutFragment.newInstance(position + 1);
-				break;
 			case 1:
-				f = AgendaFragment.newInstance(position + 1);
+				f = AboutFragment.newInstance(position);
 				break;
 			case 2:
+				f = AgendaFragment.newInstance(position);
 				break;
 			case 3:
+				f = WallFragment.newInstance(position);
 				break;
 			case 4:
+				break;
+			case 5:
 				break;
 		}
 		fragmentManager.beginTransaction().replace(R.id.container, f).commit();
