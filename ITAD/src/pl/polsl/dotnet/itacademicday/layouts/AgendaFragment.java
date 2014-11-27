fbfb8ff8ea2ce05b77ab5d6c9e1c84d4056301af
RedScorpio <krzysiekmie@gmail.com>
2014-11-27 22:10:37 +0100

    Layout tweaked. Bitmap cache. Only Agenda list is ready. I'll do the rest tomorrow.

--- ITAD/src/pl/polsl/dotnet/itacademicday/layouts/AgendaFragment.java
+++ ITAD/src/pl/polsl/dotnet/itacademicday/layouts/AgendaFragment.java
@@ -7,8 +7,11 @@ import pl.polsl.dotnet.itacademicday.core.DataFactory;
 import pl.polsl.dotnet.itacademicday.core.entities.LecturesEntity;
 import pl.polsl.dotnet.itacademicday.layouts.MainActivity.FontStyle;
 import pl.polsl.dotnet.itacademicday.utils.Bitmaps;
+import pl.polsl.dotnet.itacademicday.utils.Bitmaps.RequestResult;
+import pl.polsl.dotnet.itacademicday.utils.WorkerThread;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.Fragment;
@@ -22,21 +25,23 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class AgendaFragment extends Fragment {
 	/**
-	 * The fragment argument representing the section number for this
-	 * fragment.
+	 * The fragment argument representing the section number for this fragment.
 	 */
 	private static final String ARG_SECTION_NUMBER = "section_number";
 
 	/**
-	 * Returns a new instance of this fragment for the given section
-	 * number.
+	 * Returns a new instance of this fragment for the given section number.
 	 */
-	public static AgendaFragment newInstance(int sectionNumber){
+	public static AgendaFragment newInstance(int sectionNumber) {
 		AgendaFragment fragment = new AgendaFragment();
 		Bundle args = new Bundle();
 		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
@@ -48,7 +53,7 @@ public class AgendaFragment extends Fragment {
 	}
 
 	private LayoutInflater mInflater;
-	private ListView mList;
+	private PullToRefreshListView mList;
 	private AgendaAdapter mAdapter;
 	private ProgressBar mProgressBar;
 
@@ -57,7 +62,7 @@ public class AgendaFragment extends Fragment {
 
 		private class LectureViewTag {
 			ImageView iconView;
-			TextView nameView, lecturerView;
+			TextView nameView, lecturerView, timeView;
 		}
 
 		public AgendaAdapter() {
@@ -65,26 +70,26 @@ public class AgendaFragment extends Fragment {
 		}
 
 		@Override
-		public int getCount(){
+		public int getCount() {
 			return mAgendaList.size();
 		}
 
 		@Override
-		public LecturesEntity getItem(int position){
+		public LecturesEntity getItem(int position) {
 			return mAgendaList.get(position);
 		}
 
 		@Override
-		public long getItemId(int position){
+		public long getItemId(int position) {
 			return position;
 		}
 
-		public void setData(ArrayList<LecturesEntity> l){
+		public void setData(ArrayList<LecturesEntity> l) {
 			if (l != null) {
 				mAgendaList = l;
 				getActivity().runOnUiThread(new Runnable() {
 					@Override
-					public void run(){
+					public void run() {
 						notifyDataSetChanged();
 					}
 				});
@@ -92,89 +97,129 @@ public class AgendaFragment extends Fragment {
 		}
 
 		@Override
-		public View getView(int position, View convertView, ViewGroup parent){
+		public View getView(int position, View convertView, ViewGroup parent) {
 			LectureViewTag t;
 			if (convertView == null) {
-				ViewGroup v = (ViewGroup) mInflater.inflate(R.layout.lecture, mList, false);
+				ViewGroup v = (ViewGroup) mInflater.inflate(R.layout.lecture,
+						mList, false);
 				MainActivity.setFont(v, FontStyle.SEMILIGHT);
 				t = new LectureViewTag();
 				t.iconView = (ImageView) v.findViewById(R.id.icon);
 				t.nameView = (TextView) v.findViewById(R.id.name);
 				t.lecturerView = (TextView) v.findViewById(R.id.lecturer);
+				t.timeView = (TextView) v.findViewById(R.id.time);
 				v.setTag(t);
 				convertView = v;
 			} else {
 				t = (LectureViewTag) convertView.getTag();
 			}
 			LecturesEntity l = getItem(position);
-			Bitmaps.loadNetBitmapAsync(l.getIconURL(), t.iconView.getWidth(), t.iconView.getHeight())
-					.result(t.iconView).start();
+			final ImageView iv = t.iconView;
+			Bitmaps.loadNetBitmapAsync(l.getIconURL(), t.iconView.getWidth(),
+					t.iconView.getHeight(), true).result(new RequestResult() {
+				@Override
+				public void onBitmap(final Bitmap b) {
+					mList.post(new Runnable() {
+						@Override
+						public void run() {
+							iv.setImageBitmap(b);
+							iv.invalidate();
+						}
+					});
+				}
+			}).start();
 			t.nameView.setText(l.getName());
 			t.lecturerView.setText(l.getLecturer());
+			t.timeView.setText(l.getStartTimeText() + " - "
+					+ l.getEndTimeText());
 
 			return convertView;
 		}
 	}
 
-	private void setIsLoading(final boolean isLoading){
+	private void setIsLoading(final boolean isLoading) {
 		getActivity().runOnUiThread(new Runnable() {
-
 			@Override
-			public void run(){
-				mProgressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+			public void run() {
+				mProgressBar
+						.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+				mList.setRefreshing(isLoading);
+				if (!isLoading) {
+					mAdapter.notifyDataSetChanged();
+				}
 			}
 		});
 	}
 
-	private void errorToast(final Exception e){
+	private void errorToast(final Exception e) {
 		getActivity().runOnUiThread(new Runnable() {
-
 			@Override
-			public void run(){
-				Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+			public void run() {
+				Toast.makeText(getActivity(), "Error: " + e.getMessage(),
+						Toast.LENGTH_LONG).show();
 			}
 		});
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		mInflater = inflater;
-		View rootView = inflater.inflate(R.layout.agenda_fragment, container, false);
+		View rootView = inflater.inflate(R.layout.agenda_fragment, container,
+				false);
 		MainActivity.setFont(rootView, FontStyle.REGULAR);
-		mList = (ListView) rootView.findViewById(R.id.agenda_list);
+		MainActivity.setFont(rootView.findViewById(R.id.subtitle),
+				FontStyle.LIGHT);
+
+		mList = (PullToRefreshListView) rootView.findViewById(R.id.agenda_list);
+		mList.setOnRefreshListener(new OnRefreshListener<ListView>() {
+			@Override
+			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+				loadData();
+				Toast.makeText(mList.getContext(), "d", Toast.LENGTH_LONG)
+						.show();
+			}
+		});
 		mProgressBar = (ProgressBar) rootView.findViewById(R.id.progress_bar);
 		mAdapter = new AgendaAdapter();
 		mList.setAdapter(mAdapter);
-		Thread thread = new Thread(new Runnable() {
-
-			@SuppressLint("NewApi")
-			@Override
-			public void run(){
-				setIsLoading(true);
-				try {
-					if (android.os.Build.VERSION.SDK_INT > 9) {
-						StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-						StrictMode.setThreadPolicy(policy);
-					}
-					mAdapter.setData(DataFactory.getLecturesData());
-				} catch (Exception e) {
-					e.printStackTrace();
-					errorToast(e);
-				}
-				setIsLoading(false);
-			}
-		});
-		thread.start();
+		loadData();
 		return rootView;
 	}
 
+	private Runnable mLoadDataRunnable = new Runnable() {
+		@SuppressLint("NewApi")
+		@Override
+		public void run() {
+			setIsLoading(true);
+			try {
+				if (android.os.Build.VERSION.SDK_INT > 9) {
+					StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+							.permitAll().build();
+					StrictMode.setThreadPolicy(policy);
+				}
+				mAdapter.setData(DataFactory.getLecturesData());
+			} catch (Exception e) {
+				e.printStackTrace();
+				errorToast(e);
+			}
+			setIsLoading(false);
+		}
+	};
+
+	private void loadData() {
+		WorkerThread worker = MainActivity.getWorker();
+		worker.abortAllTasks();
+		worker.addTask(mLoadDataRunnable);
+	}
+
 	@Override
-	public void onAttach(Activity activity){
+	public void onAttach(Activity activity) {
 		super.onAttach(activity);
 	}
 
 	@Override
-	public void onDetach(){
+	public void onDetach() {
 		super.onDetach();
 	}
 }
--- ITAD/src/pl/polsl/dotnet/itacademicday/layouts/MainActivity.java
+++ ITAD/src/pl/polsl/dotnet/itacademicday/layouts/MainActivity.java
@@ -1,131 +1,180 @@
 package pl.polsl.dotnet.itacademicday.layouts;
 
+import java.io.File;
+
 import pl.polsl.dotnet.itacademicday.R;
 import pl.polsl.dotnet.itacademicday.core.DataFactory;
+import pl.polsl.dotnet.itacademicday.utils.Bitmaps;
+import pl.polsl.dotnet.itacademicday.utils.WorkerThread;
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.Typeface;
-import android.graphics.drawable.ColorDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.readystatesoftware.systembartint.SystemBarTintManager;
-
-public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends ActionBarActivity implements
+		NavigationDrawerFragment.NavigationDrawerCallbacks {
 
 	/**
-	 * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+	 * Fragment managing the behaviors, interactions and presentation of the
+	 * navigation drawer.
 	 */
 	private NavigationDrawerFragment mNavigationDrawerFragment;
 
 	/**
-	 * Used to store the last screen title. For use in {@link #makeActionBar()}.
+	 * Used to store the last screen title. For use in {@link #refreshHeader()}.
 	 */
 	private static int mColor;
 
 	private static ConnectivityManager mConManager;
-	private SystemBarTintManager mTintManager;
 	private static Typeface regular, light, semilight;
+	private ViewGroup mHeader;
 	private TextView mTitleView;
+	private static WorkerThread mWorker;
+	private static File mCacheDir;
 
-	public static int getCurrentColor(){
+	public static WorkerThread getWorker() {
+		if (mWorker == null) {
+			mWorker = new WorkerThread("DataWorkerThread");
+		}
+		return mWorker;
+	}
+
+	public static int getCurrentColor() {
 		return mColor;
 	}
 
-	public static boolean hasAccessToNetwork(){
+	public static boolean hasAccessToNetwork() {
 		final NetworkInfo activeNetwork = mConManager.getActiveNetworkInfo();
 		return (activeNetwork != null && activeNetwork.isConnected());
 	}
 
+	public static File getDiskCacheDir() {
+		return mCacheDir;
+	}
+
+	@SuppressLint({ "NewApi" })
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		DataFactory.setSharedPreferences(getPreferences(0));
+		final String cachePath = ((Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED || !Environment
+				.isExternalStorageRemovable()) && getExternalCacheDir() != null) ? getExternalCacheDir()
+				.getPath() : getCacheDir().getPath();
+		mCacheDir = new File(cachePath);
+		Bitmaps.setup();
+
+		if (Build.VERSION.SDK_INT < 16) {
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+					WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		} else {
+			View decorView = getWindow().getDecorView();
+			decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+		}
 		setContentView(R.layout.activity_main);
-		
-		mTintManager = new SystemBarTintManager(this);
-		mTintManager.setStatusBarTintEnabled(true);
+		ActionBar ab = getSupportActionBar();
+		if (ab != null)
+			ab.hide();
+		mHeader = (ViewGroup) findViewById(R.id.action_bar_layout);
+		mTitleView = (TextView) mHeader.findViewById(R.id.action_bar_title);
+		mTitleView.setTypeface(light);
+		final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		ImageButton drawerButton = (ImageButton) mHeader
+				.findViewById(R.id.action_bar_drawer_button);
+		drawerButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				drawerLayout.openDrawer(Gravity.LEFT);
+			}
+		});
 
 		mConManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		regular = Typeface.createFromAsset(getAssets(), "Segoe.ttf");
 		light = Typeface.createFromAsset(getAssets(), "SegoeLight.ttf");
 		semilight = Typeface.createFromAsset(getAssets(), "SegoeSemilight.ttf");
-		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(
-				R.id.navigation_drawer);
-		mTitleView = new TextView(this);
-		mTitleView.setText(getString(R.string.app_name));
-		mTitleView.setTypeface(semilight);
-		mTitleView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.app_title_font));
-		mTitleView.setTextColor(Color.WHITE);
-		mTitleView.setPadding((int) getResources().getDimension(R.dimen.title_padding), 0, 0, 0);
+		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.navigation_drawer);
+
 		// Set up the drawer.
-		mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
+				(DrawerLayout) findViewById(R.id.drawer_layout));
 
 	}
 
 	@Override
-	protected void onResume(){
+	protected void onResume() {
 		super.onResume();
 		if (mConManager == null)
 			mConManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 	}
 
 	@Override
-	public void onNavigationDrawerItemSelected(int position){
+	protected void onStop() {
+		if (mWorker != null) {
+			mWorker.abortAllTasks();
+			mWorker.interrupt();
+			mWorker = null;
+		}
+		super.onStop();
+	}
+
+	@Override
+	public void onNavigationDrawerItemSelected(int position) {
 		// update the main content by replacing fragments
 		FragmentManager fragmentManager = getSupportFragmentManager();
 		position++;
 		Fragment f = null;
 		switch (position) {
-			case 1:
-				f = AboutFragment.newInstance(position);
-				mColor = getResources().getColor(R.color.section1);
-				break;
-			case 2:
-				f = AgendaFragment.newInstance(position);
-				mColor = getResources().getColor(R.color.section2);
-				break;
-			case 3:
-				f = WallFragment.newInstance(position);
-				mColor = getResources().getColor(R.color.section3);
-				break;
-			case 4:
-				f = SponsorsFragment.newInstance(position);
-				mColor = getResources().getColor(R.color.section4);
-				break;
-			case 5:
-				break;
+		case 1:
+			f = AboutFragment.newInstance(position);
+			mColor = getResources().getColor(R.color.section1);
+			break;
+		case 2:
+			f = AgendaFragment.newInstance(position);
+			mColor = getResources().getColor(R.color.section2);
+			break;
+		case 3:
+			f = WallFragment.newInstance(position);
+			mColor = getResources().getColor(R.color.section3);
+			break;
+		case 4:
+			f = SponsorsFragment.newInstance(position);
+			mColor = getResources().getColor(R.color.section4);
+			break;
+		case 5:
+			break;
 		}
-		makeActionBar();
+		refreshHeader();
 		fragmentManager.beginTransaction().replace(R.id.container, f).commit();
 	}
 
-	public void makeActionBar(){
-		ActionBar actionBar = getSupportActionBar();
-		actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM, ActionBar.DISPLAY_SHOW_CUSTOM);
-		actionBar.setDisplayShowTitleEnabled(false);
-		actionBar.setCustomView(mTitleView);
-		actionBar.setBackgroundDrawable(new ColorDrawable(mColor));
-		if (mTintManager != null) {
-			mTintManager.setStatusBarTintColor(mColor);
+	public void refreshHeader() {
+		if (mHeader != null) {
+			mHeader.setBackgroundColor(mColor);
 		}
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu){
+	public boolean onCreateOptionsMenu(Menu menu) {
 		if (!mNavigationDrawerFragment.isDrawerOpen()) {
-			makeActionBar();
+			refreshHeader();
 			return true;
 		}
 		return super.onCreateOptionsMenu(menu);
@@ -135,7 +184,7 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 		REGULAR, LIGHT, SEMILIGHT
 	};
 
-	public static void setFont(View v, FontStyle style){
+	public static void setFont(View v, FontStyle style) {
 		if (v instanceof ViewGroup) {
 			ViewGroup vg = (ViewGroup) v;
 			int numChildren = vg.getChildCount();
@@ -145,16 +194,16 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 		} else if (v instanceof TextView) {
 			TextView tv = (TextView) v;
 			switch (style) {
-				case LIGHT:
-					tv.setTypeface(light);
-					break;
-				case SEMILIGHT:
-					tv.setTypeface(semilight);
-					break;
-				case REGULAR:
-				default:
-					tv.setTypeface(regular);
-					break;
+			case LIGHT:
+				tv.setTypeface(light);
+				break;
+			case SEMILIGHT:
+				tv.setTypeface(semilight);
+				break;
+			case REGULAR:
+			default:
+				tv.setTypeface(regular);
+				break;
 			}
 
 		}
