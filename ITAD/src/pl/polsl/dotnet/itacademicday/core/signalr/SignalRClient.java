39c9b815ea14cb0e9f8ffb89a2b4ae0af577f899
Przemyslaw Kaczmarczyk <przemas55555@gmail.com>
2014-11-29 14:32:26 +0100

    signalR client works

--- ITAD/src/pl/polsl/dotnet/itacademicday/core/signalr/SignalRClient.java
+++ ITAD/src/pl/polsl/dotnet/itacademicday/core/signalr/SignalRClient.java
@@ -1,56 +1,65 @@
 package pl.polsl.dotnet.itacademicday.core.signalr;
 
-import com.google.gson.JsonElement;
+import java.util.ArrayList;
 
+import microsoft.aspnet.signalr.client.Action;
 import microsoft.aspnet.signalr.client.LogLevel;
 import microsoft.aspnet.signalr.client.Logger;
-import microsoft.aspnet.signalr.client.MessageReceivedHandler;
 import microsoft.aspnet.signalr.client.Platform;
-import microsoft.aspnet.signalr.client.SignalRFuture;
 import microsoft.aspnet.signalr.client.hubs.HubConnection;
 import microsoft.aspnet.signalr.client.hubs.HubProxy;
-import microsoft.aspnet.signalr.client.hubs.SubscriptionHandler1;
 
 public class SignalRClient {
 
 	protected HubProxy hub;
 
 	protected HubConnection connection;
+	private static SignalRClient Instance = null;
 
-	private SignalRFuture<Void> awaitConnection;
+	private ArrayList<String> wallInfo = new ArrayList<String>();
 
 	private Logger logger = new Logger() {
 
 		@Override
-		public void log(String message, LogLevel level){
+		public void log(String message, LogLevel level) {
 			System.out.println(message);
 		}
 	};
 
-	public SignalRClient(final OnMessage onMsg) {
+	private SignalRClient() {
 		Platform.loadPlatformComponent(new AndroidPlatformComponent());
 		// Change to the IP address and matching port of your SignalR server.
 		String host = "http://itadpolsl.pl/signalr";
 		HubConnection connection = new HubConnection(host, "", true, logger);
 
-		hub = connection.createHubProxy("wallhub");
-		awaitConnection = connection.start();
-		connection.received(new MessageReceivedHandler() {
-			
+		hub = connection.createHubProxy("wallHub");
+		hub.subscribe(new Object() {
+			@SuppressWarnings("unused")
+			public void posted(String message) {
+				wallInfo.add(message);
+			}
+		});
+
+		connection.start().done(new Action<Void>() {
 			@Override
-			public void onMessageReceived(JsonElement json) {
-				onMsg.onMessage(json.toString());
-				
+			public void run(Void obj) throws Exception {
+				hub.invoke("claimParticipant");
 			}
 		});
 	}
 
-	public boolean sendMessage(String message){
+	public boolean sendMessage(String message) {
 		return hub.invoke("Post", message).isDone();
 	}
 
-	public interface OnMessage {
-		public void onMessage(String message);
+	public static SignalRClient getInstance() {
+		if (Instance == null) {
+			Instance = new SignalRClient();
+		}
+		return Instance;
 	}
 
+	public ArrayList<String> getWallInfo() {
+		return wallInfo;
+	}
 }
